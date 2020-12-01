548c8d02bb60eb945977fcc8bce8f0a9cfb773a7
jungwoni4080 <doge4080@gmail.com>
2020-12-01 17:13:57 +0900

    리액트 전

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const { User } = require("./models/User");
 const config = require('./config/key');
 const cookieParsre = require('cookie-parser'); 
 
+const { auth } = require('./middleware/auth');
+
+
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -56,7 +59,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
 
     //요청된 이메일을 db에서 있는지 찾는다. 
@@ -64,13 +67,13 @@ app.post('/login', (req, res) => {
         if(!user) {
             return res.json({
                 loginSuccess: false,
-                message: "제공된 이메일을 가진 유저는 없습니다."
+                message: "제공된 이메일을 가진 유저는 없습니다. No email user"
             })
         }
         //요청된 이메일이 db에 있다면, 비밀번호가 맞는 비밀번호인지 확인
-        user.comParePassword(req.body.password, (err, isMatch) => {
+        user.comparePassword(req.body.password, (err, isMatch) => {
             if(!isMatch) 
-                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다. Incorrect PAssword"})
             
             //비밀번호가 맞다면, 토큰을 생성함
             user.generateToken((err, user) => {
@@ -86,6 +89,45 @@ app.post('/login', (req, res) => {
     })
 })
 
+/*
+app.get('/api/users/auth',auth, (req,res) => {            //auth -- /auth한 후 (Req,res) 하기 전에 하는 것
+    
+    //여기까지 미들웨어를 통과해 왓다는 얘기는 authentification이 true라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0? false : true,  
+        isAuth:true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })  
+})
+*/
+app.get('/api/users/auth', auth, (req, res) => {
+    //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
+    res.status(200).json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      lastname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image
+    })
+  })
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({_id:req.user._id}, 
+    {token :""} //토큰은 없어져
+    , (err,user) => {
+        if(err) return res.json({success: false, err});
+        return res.status(200).send({
+            success:true
+        })
+    })
+})
 
 
 
--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        maxlength: 60
     },
     role: {
         type: Number,
@@ -61,24 +61,41 @@ userSchema.pre('save', function( next ){
 userSchema.methods.comparePassword = function(plainPassword, cb){
     //plainPassword 1234567 암호화된 비밀번호 ~~~~~~blabla
     bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if (err) return cb(err),
+        if (err) return cb(err);
             cb(null, isMatch)   
     })
 }
 
 userSchema.methods.generateToken = function(cb) {
     
-    var user = this
+    var user = this;
 
     //jsonwebToken을 이용해서 토큰을 생성하기 
-    var token = jwt.sign(uswer._id, 'secretToken')
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
 
     user.token = token
     user.save(function(err,user) {
-        if(err) return cb(error)
+        if(err) return cb(err)
         cb(null, user)
     })
     
+}
+
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    
+    //토큰을 decode 함
+
+    jwt.verify(token, 'secretToken', function(err,decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음
+        //클라이언트에서 가져온 토큰과 데이터베이스에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null,user)
+        })
+    })
+
 }
 
 
